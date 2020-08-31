f5a9f2e892fc966d116c400e293e2c09aa54e0f6
Kerollos Fahmy <Kerollos.Fahmy@integrant.com>
2020-08-31 14:12:55 +0200

    Some RxJs Operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { of, from, interval, fromEvent, Observable, Subscription } from 'rxjs';
+import { take, map, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -32,10 +33,27 @@ export class AppComponent implements OnInit {
     let subscription4 = of([9, 8, 7, 6, 5]).subscribe(console.log); // emit only one element => emit the array as one element 
 
     let subscription5 = interval(100).subscribe(console.log);     // async, emit incremental numbers with 100 ms interval between them 
-    setTimeout(() => subscription5.unsubscribe(), 400);           // emit forever till unsubscribe
+    setTimeout(() => subscription5.unsubscribe(), 400);           // emit forever till unsubscribe, or stop emitting the observable stream by any other way
 
     // fromEvent(element, eventName).subscribe()
     //#endregion
+
+    //#region some RxJs operators 
+    console.log(".................... operators ..................");
+    of(2, 4, 6, 8, 10)
+      .pipe(
+        take(2),
+        take(3),                                                // multiple take will choose the smallest one 
+        tap((item) => console.log(`emitted item before map: ${item}`)),
+        map(item => item * 2),
+        tap((item) => console.log(`emitted item after map: ${item}`)),
+      )
+      .subscribe(
+        element => console.log(`next: ${element}`),
+        err => console.log(err),
+        () => console.log("operators completed..."));
+    //#endregion
+
   }
 
 
